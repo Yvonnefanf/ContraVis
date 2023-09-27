305808c851ab434ed8e3b2c7693f13bf71cc7dff
Yvonnefanf <m18522608197@163.com>
2023-09-27 18:26:59 +0800

    add

---
+++ Baseline/Manifold-alignedNeighborEmbedding/MANE.py
@@ -0,0 +1,276 @@
+import numpy as np
+
+import numba
+from numba import prange
+
+import random
+
+import scipy.sparse
+
+import gc
+
+
+@numba.jit(nopython=True, parallel=True)
+def euclidean_distances_numba(X, squared = True):
+    n = X.shape[0]
+    xcorr = np.zeros((n,n),dtype=X.dtype)
+    for i in prange(n):
+        for j in range(i,n):
+            dist = np.sum( np.square(X[i,:] - X[j,:]) )
+            if not squared:
+                dist = np.sqrt(dist)
+            xcorr[i,j] = dist
+            xcorr[j,i] = dist
+    
+    return xcorr
+
+#@numba.jit(nopython=True)
+def get_weight_function(dists, rho, sigma):
+    d = dists - rho
+    #print(d)
+    d[d<0] = 0
+    weight = np.exp(- d / sigma )
+    return weight
+
+#@numba.jit(nopython=True)
+def search_sigma(dists, rho, k, tol = 10**-5, n_iteration=200):
+    sigma_min = 0
+    sigma_max = 1000
+    
+    cur_sigma = 100
+    
+    logk = np.log2(k)
+    #print(logk)
+    
+    for i in range(n_iteration):
+        
+        cur_sigma = (sigma_min+sigma_max)/2
+        probs = get_weight_function(dists,rho,cur_sigma)
+        weight = np.sum(probs)
+        #print(weight)
+        
+        if np.abs(logk - weight) < tol:
+            break
+        
+        if weight < logk:
+            sigma_min = cur_sigma
+        else:
+            sigma_max = cur_sigma
+        
+    return cur_sigma, probs
+
+@numba.jit(nopython=True, parallel=True)
+def symmetrization_step(prob):
+    n = prob.shape[0]
+    P = np.zeros((n,n),dtype=np.float32)
+
+    for i in prange(n):
+        #if i%1000 == 0:
+        #    print('Completed ', i, ' of ', n)
+        for j in prange(i,n):
+            p = prob[i,j] + prob[j,i] - prob[i,j] * prob[j,i] #t-conorm
+            P[i,j] = p
+            P[j,i] = p
+            
+    return P
+
+def get_prob_matrix(X, n_neighbors=15):
+    n = X.shape[0]
+    dist = euclidean_distances_numba(X, squared = False)
+    sort_idx = np.argsort(dist,axis=1)
+    #sort_idx = sort_idx.astype(np.int32)
+    sort_idx = sort_idx[:,1:n_neighbors+1]
+    
+    rho = [ dist[i, sort_idx[i,0] ] for i in range(n)]
+    rho = np.array(rho)
+    
+    
+
+    sigmas = []
+
+    directed_graph = []
+
+
+    #'''
+    for i in range(n):
+        if (i+1)%1000 == 0:
+            print('Processed ', i+1, ' of ', n, ' samples.')
+        sigma, weights = search_sigma(dists = dist[i,sort_idx[i,:]],rho = rho[i],k = n_neighbors)
+
+        probs = np.zeros(n)
+        probs[sort_idx[i,:]] = weights
+        #print(sum(weights), np.log2(n_neighbors))
+        #print(sort_idx[i,:])
+        #print(probs[1770:1780])
+
+        directed_graph.append(probs)
+
+    directed_graph = np.array(directed_graph).astype(np.float32)
+    prob = directed_graph
+    
+    P = symmetrization_step(prob)
+    
+    graph = scipy.sparse.coo_matrix(P)
+    
+    return graph
+
+def make_epochs_per_sample(weights, n_epochs):
+    """Given a set of weights and number of epochs generate the number of
+    epochs per sample for each weight.
+    Parameters
+    ----------
+    weights: array of shape (n_1_simplices)
+        The weights ofhow much we wish to sample each 1-simplex.
+    n_epochs: int
+        The total number of epochs we want to train for.
+    Returns
+    -------
+    An array of number of epochs per sample, one for each 1-simplex.
+    Copied from UMAP repo: https://github.com/lmcinnes/umap/
+    """
+    result = -1.0 * np.ones(weights.shape[0], dtype=np.float64)
+    n_samples = n_epochs * (weights / weights.max())
+    result[n_samples > 0] = float(n_epochs) / n_samples[n_samples > 0]
+    return result
+    
+
+
+
+
+@numba.jit(nopython=True)
+def clip(x,val=4.0):
+
+    if x>val:
+        return val
+    elif x<-val:
+        return -val
+    else:
+        return x
+    
+@numba.jit(nopython=True)
+def update_attraction(x, y, a, b, dim, lr, P):
+    dist = np.sum((x - y)**2)
+
+    if dist>0.0:
+        grad_coeff = 2*a*b*dist**(b-1.0) / (1 + a * dist**b)
+    else:
+        grad_coeff = 0.0
+
+
+    for d in range(dim):
+        mv = clip(grad_coeff * P * (x[0,d]-y[0,d]))  # * P[idx,idy]
+        mv = mv * lr
+
+        x[0,d] -= mv
+        y[0,d] += mv
+        
+    return
+
+@numba.jit(nopython=True)
+def update_repulsion(x, y, a, b, dim, lr, P,repulsion_strength=1.0):
+    dist = np.sum((x - y)**2)
+
+    if dist>0.0:
+        grad_coeff = 2 * repulsion_strength * b / ( (0.001+dist) * (1.0 + a * dist**b) )
+    else:
+        grad_coeff = 0
+
+
+    for d in range(dim):
+        #if grad_coeff > 0.0:
+        #    grad = clip(grad_coeff  * (x[0,d]-y[0,d]))
+        #    #* (1 - P[idx,idy])
+        #else:
+        #    grad = 0.0
+
+        grad = clip(grad_coeff  * (x[0,d]-y[0,d]) * (1-P))
+        mv = grad * lr
+
+        x[0,d] += mv
+        #y[0,d] -= mv
+
+    
+    
+@numba.jit(nopython=True)
+def one_step_in_a_set(emA, emH, idx, rows, columns, a, b, dim,
+                   nA, n_points,
+                   epochs_per_sample,
+                   epoch_of_next_sample,
+                   epochs_per_negative_sample,
+                   epoch_of_next_negative_sample,
+                   lr, epoch):
+    
+    if epoch_of_next_sample[idx] <= epoch:
+        x_idx = rows[idx]
+        y_idx = columns[idx]
+        
+        if x_idx < nA:
+            x = emA[x_idx:x_idx+1,:]
+        else:
+            x = emH[x_idx-nA:x_idx-nA+1,:]
+        
+        if y_idx < nA:
+            y = emA[y_idx:y_idx+1, :]
+        else:
+            y = emH[y_idx-nA:y_idx-nA+1,:]
+            
+        update_attraction(x, y, a, b, dim, lr, 1)
+        
+        epoch_of_next_sample[idx] += epochs_per_sample[idx]
+        
+        n_neg_samples = int(
+                (epoch - epoch_of_next_negative_sample[idx]) / epochs_per_negative_sample[idx]
+            )
+        
+        for i in range(n_neg_samples):
+            y_idx = np.random.choice(n_points)
+            
+            if x_idx == y_idx:
+                continue
+            
+            if y_idx < nA:
+                y = emA[y_idx:y_idx+1, :]
+            else:
+                y = emH[y_idx-nA:y_idx-nA+1,:]
+                
+            update_repulsion(x, y, a, b, dim, lr, 0)
+            
+        epoch_of_next_negative_sample[idx] += (
+                n_neg_samples * epochs_per_negative_sample[idx]
+            )
+            
+            
+            
+    
+    return 
+
+@numba.jit(nopython=True, parallel=True)
+def one_epoch_nsets(emCommon, embS,
+                     rows, columns,
+                     nCommon, n_points_,
+                     Set, K_idx,
+                     a, b, dim,
+                     lr, epoch,
+                     epochs_per_sample,
+                     epoch_of_next_sample,
+                     epochs_per_negative_sample,
+                     epoch_of_next_negative_sample,
+                     repulsion_strength=1.0):
+    '''
+    Set1 = 1 * np.ones(epochs_per_sample_1.shape[0])
+    Set2 = 2 * np.ones(epochs_per_sample_2.shape[0])
+    Set = np.random.permutation(np.concatenate((Set1,Set2)))
+    '''
+    
+    for i in prange(len(Set)):
+        idx = Set[i]
+        one_step_in_a_set(emA=emCommon, emH=embS[idx], idx=K_idx[i], 
+                              rows=rows[idx], columns=columns[idx], a=a, b=b, dim=dim,
+                              nA=nCommon, n_points=n_points_[idx],
+                              epochs_per_sample=epochs_per_sample[idx],
+                              epoch_of_next_sample=epoch_of_next_sample[idx],
+                              epochs_per_negative_sample=epochs_per_negative_sample[idx],
+                              epoch_of_next_negative_sample=epoch_of_next_negative_sample[idx],
+                              lr=lr, epoch=epoch)
+    
+    return 
