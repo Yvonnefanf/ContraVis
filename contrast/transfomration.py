11d9465959ead4eefc75b32a3fed1d3f8a44301e
Yvonnefanf <m18522608197@163.com>
2023-10-10 20:52:52 +0800

    add new

--- contrast/transfomration.py
+++ contrast/transfomration.py
@@ -84,5 +84,55 @@ def transformation_train(ref_data,tar_data):
 
     return model, tar_mapped, ref_reconstructed
 
+"""
+advanced transformation method
+consider the nn-preserving
+"""
+def transformation_train_advanced(ref_data,tar_data):
+    # Initialize the model, loss, and optimizer
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = Autoencoder().to(device)
+
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    # Assume tar_tensor and ref_tensor are your data in PyTorch tensor format
+    tar_tensor = torch.tensor(tar_data, dtype=torch.float32).cuda()  # X_train2 was the tar in your previous example
+    ref_tensor = torch.tensor(ref_data, dtype=torch.float32).cuda()  # X_train1 was the ref in your previous example
+    tar_tensor = tar_tensor.to(device)
+    ref_tensor = ref_tensor.to(device)
+    lambda_translation = 0.5
+
+    # Train the autoencoder
+    num_epochs = 200
+    for epoch in range(num_epochs):
+        model.train()
+
+        optimizer.zero_grad()
+
+        # Forward pass
+        latent_representation = model.encoder(tar_tensor)
+        outputs = model.decoder(latent_representation)
+
+        # Compute the two losses
+        reconstruction_loss = criterion(outputs, tar_tensor)
+        translation_loss = criterion(latent_representation, ref_tensor)
+
+        # Combine the losses
+        loss = reconstruction_loss + lambda_translation * translation_loss
 
 
+        # Backward pass and optimization
+        loss.backward()
+        optimizer.step()
+
+        if epoch % 20 == 0:
+            print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}")
+
+    # To get the mapped version of tar
+    tar_mapped = model.encoder(tar_tensor).cpu().detach().numpy()
+
+    # To get ref back from the mapped tar
+    ref_reconstructed = model.decoder(torch.tensor(tar_mapped).to(device)).cpu().detach().numpy()
+
+    return model, tar_mapped, ref_reconstructed
