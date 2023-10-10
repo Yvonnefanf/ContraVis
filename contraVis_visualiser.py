7d877352c16a8138e8b7fe5fe6b5d4bf999e1c14
Yvonnefanf <m18522608197@163.com>
2023-10-10 20:29:23 +0800

    add visulizer

--- contraVis_visualiser.py
+++ contraVis_visualiser.py
@@ -134,95 +134,20 @@ recon_loss_fn = ReconstructionLoss(beta=1.0)
 single_loss_fn = SingleVisLoss(umap_loss_fn, recon_loss_fn, lambd=LAMBDA1)
 # Define Projector
 projector = DVIProjector(vis_model=model, content_path=CONTENT_PATH, vis_model_name=VIS_MODEL_NAME, device=DEVICE)
-
-start_flag = 1
-
-prev_model = VisModel(ENCODER_DIMS, DECODER_DIMS)
-
-
-
-for iteration in range(EPOCH_START, EPOCH_END+EPOCH_PERIOD, EPOCH_PERIOD):
-    # Define DVI Loss
-    if start_flag:
-        temporal_loss_fn = DummyTemporalLoss(DEVICE)
-        criterion = DVILoss(umap_loss_fn, recon_loss_fn, temporal_loss_fn, lambd1=LAMBDA1, lambd2=0.0,device=DEVICE)
-        start_flag = 0
-    else:
-        # TODO AL mode, redefine train_representation
-        prev_data = data_provider.train_representation(iteration-EPOCH_PERIOD)
-        curr_data = data_provider.train_representation(iteration)
-        npr = torch.tensor(find_neighbor_preserving_rate(prev_data, curr_data, N_NEIGHBORS)).to(DEVICE)
-        temporal_loss_fn = TemporalLoss(w_prev, DEVICE)
-        criterion = DVILoss(umap_loss_fn, recon_loss_fn, temporal_loss_fn, lambd1=LAMBDA1, lambd2=LAMBDA2*npr,device=DEVICE)
-    # Define training parameters
-    optimizer = torch.optim.Adam(model.parameters(), lr=.01, weight_decay=1e-5)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=.1)
-    # Define Edge dataset
-    t0 = time.time()
-    tar_data = tar_data_provider.train_representation(EPOCH_START)
-    tar_data = tar_data.reshape(tar_data.shape[0],tar_data.shape[1])
-    trans_model,tar_mapped,ref_reconstructed  = transformation_train(data_provider.train_representation(EPOCH_START),tar_data)
-    spatial_cons = SpitalEdgeForContrastConstructor(data_provider, iteration, S_N_EPOCHS, B_N_EPOCHS, N_NEIGHBORS,tar_mapped,tar_data_provider)
-    edge_to, edge_from, probs, feature_vectors, attention = spatial_cons.construct()
-    t1 = time.time()
-
-    probs = probs / (probs.max()+1e-3)
-    eliminate_zeros = probs> 1e-3    #1e-3
-    edge_to = edge_to[eliminate_zeros]
-    edge_from = edge_from[eliminate_zeros]
-    probs = probs[eliminate_zeros]
-    
-    dataset = DVIDataHandler(edge_to, edge_from, feature_vectors, attention)
-
-    n_samples = int(np.sum(S_N_EPOCHS * probs) // 1)
-    # chose sampler based on the number of dataset
-    if len(edge_to) > pow(2,24):
-        sampler = CustomWeightedRandomSampler(probs, n_samples, replacement=True)
-    else:
-        sampler = WeightedRandomSampler(probs, n_samples, replacement=True)
-    edge_loader = DataLoader(dataset, batch_size=2000, sampler=sampler, num_workers=8, prefetch_factor=10)
-
-    ########################################################################################################################
-    #                                                       TRAIN                                                          #
-    ########################################################################################################################
-
-    trainer = DVITrainer(model, criterion, optimizer, lr_scheduler, edge_loader=edge_loader, DEVICE=DEVICE)
-
-    t2=time.time()
-    trainer.train(PATIENT, MAX_EPOCH)
-    t3 = time.time()
-
-    # save result
-    save_dir = data_provider.model_path
-    trainer.record_time(save_dir, "time_{}".format(VIS_MODEL_NAME), "complex_construction", str(iteration), t1-t0)
-    trainer.record_time(save_dir, "time_{}".format(VIS_MODEL_NAME), "training", str(iteration), t3-t2)
-    save_dir = os.path.join(data_provider.model_path, "Epoch_{}".format(iteration))
-     ##### save the visulization model
-    trainer.save(save_dir=save_dir, file_name="{}".format(VIS_MODEL_NAME))
-    ##### save the transformation model
-    torch.save(trans_model, os.path.join(save_dir,'trans_model.m' ))
-
-
-    print("Finish epoch {}...".format(iteration))
-
-    prev_model.load_state_dict(model.state_dict())
-    for param in prev_model.parameters():
-        param.requires_grad = False
-    w_prev = dict(prev_model.named_parameters())
     
 
 ########################################################################################################################
 #                                                      VISUALIZATION                                                   #
 ########################################################################################################################
 
-# from singleVis.visualizer import visualizer
-# now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time())) 
-# vis = visualizer(data_provider, projector, 200, "tab10")
-# save_dir = os.path.join(data_provider.content_path, "img")
-# if not os.path.exists(save_dir):
-#     os.mkdir(save_dir)
-# for i in range(EPOCH_START, EPOCH_END+1, EPOCH_PERIOD):
-#     vis.savefig(i, path=os.path.join(save_dir, "{}_{}_{}_{}.png".format(DATASET, i, VIS_METHOD,now)))
+from singleVis.visualizer import visualizer
+now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time())) 
+vis = visualizer(data_provider, projector, 200, "tab10")
+save_dir = os.path.join(data_provider.content_path, "contraImg")
+if not os.path.exists(save_dir):
+    os.mkdir(save_dir)
+for i in range(EPOCH_START, EPOCH_END+1, EPOCH_PERIOD):
+    vis.savefig(i, path=os.path.join(save_dir, "{}_{}_ref.png".format(DATASET, i)))
 
     
 ########################################################################################################################
